8ffcddc65acd10aa0cd3787d0a02ad68b64e8de0
Matthias Dive <matthiasdive2@gmail.com>
2022-09-22 09:20:21 +0200

    search func

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-center items-center h-16 space-x-4 bg-black">
+    <nuxt-link to="/" class="block text-white">
+      Home
+    </nuxt-link>
+    <nuxt-link to="/" class="block text-white">
+      Search
+    </nuxt-link>
+    <nuxt-link to="/" class="block text-white">
+      Wishlist
+    </nuxt-link>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <h1>Hello World</h1>
+  <div id="app">
+    <Navbar />
+    <div class="content-center">
+      <h1>Google API Books</h1>
+      <form @submit.prevent="search">
+        <input v-model="keyword" type="text" class="input" placeholder="Search" required>
+        <input type="submit" value="Search" class="button">
+      </form>
+      <BookList v-for="book in books" :key="book.id" :book="book" />
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import Navbar from '~/components/Navbar.vue'
+import BookList from '~/components/BookList.vue'
 export default {
-  name: 'IndexPage'
+  data () {
+    return {
+      name: 'IndexPage',
+      books: [],
+      keyword: ''
+    }
+  },
+  methods: {
+    async search () {
+      const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.keyword}?projection=full`)
+      console.log(response.data)
+      this.books = response.data.items
+    }
+  },
+  components: { Navbar, BookList }
 }
 </script>
